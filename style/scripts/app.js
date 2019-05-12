bd4c3a4320a7f9db4e1015c6c3785bde958816a0
Guilherme Garber <ggarber@protonmail.com>
2019-05-12 19:43:37 -0300

    Estilo pronto

---
+++ style/scripts/app.js
@@ -0,0 +1,12 @@
+function hoverEnter(e) {
+    // background.classList.add('.hover');
+    console.log('aqui');
+}
+
+function hoverLeave() {
+    
+}
+const background = document.querySelector('.background');
+const menus = document.querySelectorAll('.bottom li');
+menus.forEach(menu => menu.addEventListener('mouseenter', hoverEnter));
+menus.forEach(menu => menu.addEventListener('mouseleave', hoverLeave));
